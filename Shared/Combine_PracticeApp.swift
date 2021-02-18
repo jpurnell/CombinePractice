cb5e48b51ea802c4b7d2250edcb53a4daeda81d7
Justin Purnell <jpurnell@mac.com>
2021-02-18 09:56:58 -0500

    Initial Commit

---
+++ Shared/Combine_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_PracticeApp.swift
+//  Shared
+//
+//  Created by Justin Purnell on 2/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Combine_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
